8695a6fa87f2ddd72c293782f4918ca9a56cc526
Ashwani <aswanibtech@gmail.com>
2025-06-20 16:02:18 +0530

    matplotlib

--- myplot.py
+++ myplot.py
@@ -1,7 +1,11 @@
-import matplotlib.pyplot as mp
+# import matplotlib.pyplot as mp
+from matplotlib.pyplot import *
 x=[1,2,3,4,5,6,7]
 y=[10,20,14,50,7,45,18]
-mp.plot(x,y)
-mp.ylabel('temperature')
-mp.xlabel('day')
-mp.show()
+# mp.plot(x,y)
+# mp.ylabel('temperature')
+# mp.xlabel('day')
+plot(x,y)
+ylabel('temperature')
+xlabel('day')
+show()
